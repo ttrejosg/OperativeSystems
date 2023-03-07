1d73fd1926a5a71901e5909763dd7c4b342969d3
Carlos Paez <carlosa.paezc@autonoma.edu.co>
2023-03-07 12:23:31 -0500

    Main refactorization

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,41 @@ import javafx.scene.Scene;
 import javafx.stage.StageStyle;
 
 public class Main extends Application{
-    private double x, y;
+    private double mouseX, mouseY;
+
     @Override
-    public void start(Stage stage) throws Exception {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("GUI/MainPage.fxml"));
+    public void start(Stage stage){
+        Scene mainPageScene = this.loadScene("GUI/MainPage.fxml");
+        stage.setScene(mainPageScene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        this.setMouseEvents(stage);
+        stage.show();
+    }
+
+    private Scene loadScene(String fxmlPath){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
             Parent root = loader.load();
             Scene scene = new Scene(root);
             scene.setFill(Color.TRANSPARENT);
-            stage.setScene(scene);
-            stage.initStyle(StageStyle.TRANSPARENT);
-            root.setOnMousePressed(event -> {
-                x = event.getSceneX();
-                y = event.getSceneY();
-            });
-            root.setOnMouseDragged(event -> {
-
-                stage.setX(event.getScreenX() - x);
-                stage.setY(event.getScreenY() - y);
-
-            });
-
-            stage.show();
-        }catch(Exception e){
+            return scene;
+        }catch (Exception e){
+            System.out.println("Scene Loading Error");
             e.printStackTrace();
+            return null;
         }
+    }
+
+    private void setMouseEvents(Stage stage){
+        Parent root = stage.getScene().getRoot();
+        root.setOnMousePressed(event -> {
+            this.mouseX = event.getSceneX();
+            this.mouseY = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - this.mouseX);
+            stage.setY(event.getScreenY() - this.mouseY);
+        });
     }
 
     public static void main(String[] args) {
