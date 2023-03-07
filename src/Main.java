e580afaa8d4c60d4f1dd6cad8be7aa3bfcc3dd1a
Carlos Paez <carlosa.paezc@autonoma.edu.co>
2023-03-07 15:08:51 -0500

    Code Refactorization

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,20 @@
+import GUI.GuiUtils;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.stage.StageStyle;
 
 public class Main extends Application{
-    private double mouseX, mouseY;
 
     @Override
     public void start(Stage stage){
-        Scene mainPageScene = this.loadScene("GUI/MainPage.fxml");
+        Scene mainPageScene = GuiUtils.loadSceneFrom("MainPage");
         stage.setScene(mainPageScene);
         stage.initStyle(StageStyle.TRANSPARENT);
-        this.setMouseEvents(stage);
+        GuiUtils.setDragAndDropOnStage(stage);
         stage.show();
     }
 
-    private Scene loadScene(String fxmlPath){
-        try{
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
-            Parent root = loader.load();
-            Scene scene = new Scene(root);
-            scene.setFill(Color.TRANSPARENT);
-            return scene;
-        }catch (Exception e){
-            System.out.println("Scene Loading Error");
-            e.printStackTrace();
-            return null;
-        }
-    }
+    public static void main(String[] args) {launch(args);}
 
-    private void setMouseEvents(Stage stage){
-        Parent root = stage.getScene().getRoot();
-        root.setOnMousePressed(event -> {
-            this.mouseX = event.getSceneX();
-            this.mouseY = event.getSceneY();
-        });
-        root.setOnMouseDragged(event -> {
-            stage.setX(event.getScreenX() - this.mouseX);
-            stage.setY(event.getScreenY() - this.mouseY);
-        });
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
